b31f559982f4b435b585922225e3eb65383c2612
Victor Oliveira <vitchor@gmail.com>
2013-09-25 17:26:32 -0300

    <SD> Added httpresponse import

--- tf/s/views.py
+++ tf/s/views.py
@@ -4,7 +4,7 @@ import sys
 # from django.db.models import Q
 # from django.utils import timezone
 # from django.utils import simplejson as json
-# from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 # from s.models import User, Picture, Featured_Picture
 # from boto.s3.connection import S3Connection
