039f205651438ce6f045ed6a3baaaaeb71beb132
Victor Oliveira <vitchor@gmail.com>
2013-09-25 17:27:48 -0300

    <SD> Added more imports

--- tf/s/views.py
+++ tf/s/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 import sys
 # import os
-# from django.db.models import Q
-# from django.utils import timezone
-# from django.utils import simplejson as json
+from django.db.models import Q
+from django.utils import timezone
+from django.utils import simplejson as json
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 # from s.models import User, Picture, Featured_Picture
